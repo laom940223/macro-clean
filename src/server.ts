245e37afe55a64950a27cea95739d07e1bd379c0
unknown <laom940223@gmail.com>
2022-06-04 21:17:48 -0400

    Added docker-compose setup for postgre and redis

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 
 
 import express, { Response } from 'express'
-import { apiRouter } from './api/api-router'
-import { MainErrorHandler } from './errors/global-error-handler'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app = express()
 
-app.use("/api", apiRouter)
-
-
-app.use(MainErrorHandler)
 
 app.listen(
     process.env.PORT,
